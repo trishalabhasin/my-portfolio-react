32a40a011a258c1361fb69caed7b02197294a624
Trishala Bhasin <37555884+trishalabhasin@users.noreply.github.com>
2021-07-21 12:06:46 -0700

    WIP

---
+++ README.md
@@ -0,0 +1,8 @@
+# Run Instructions
+
+1. Clone/Download the repo.
+2. Run  ``` npm install ```.
+. Run ```npm start``` to start the local dev server port 3000.(http://localhost:3000).
+
+
+## Work in progress...
---
+++ src/resumeData.js
@@ -0,0 +1,118 @@
+let resumeData = {
+    "imagebaseurl":"https://trishalabhasin.github.io/",
+    "name": " Trishala Bhasin",
+    "role": "Software Developer and Data Scientist",
+    "linkedinId":"Trishala Bhasin",
+    "skypeid": "trishalabhasin",
+    "roleDescription": "I like playing around with front-end of websites and conduct user research in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/trishala-bhasin-144060bb/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/trishalabhasin",
+          "className":"fa fa-github"
+        },
+
+      ],
+    "aboutme":"I am graduating student from University of Victoria with a 3 year research degree in human aspects of social software development. I want to pursue a career in Software Development and I have an interest in Data Science.",
+    "address":"Vancouver, British Columbia, Canada",
+    "website":"https://github.com/trishalabhasin",
+    "education":[
+      {
+        "UniversityName":"University of Victoria",
+        "specialization":"MSc Computer Science",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2021",
+        "Achievements":"Published my research in a leading journal of social software development. Received research grants from NSCERC for exceptional research and two consecutive scholarships for high grades."
+      },
+      {
+        "UniversityName":"Guru Gobind Singh Indraprastha University",
+        "specialization":"Bachelor of Technology",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2018",
+        "Achievements":"Head of the debate society of my university, and completed a fellowship program in Bulgaria as a Computer Vision developer"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"University of Victoria",
+        "specialization":"Graduate Research Assistant",
+        "MonthOfLeaving":"Sept 2018 - July",
+        "YearOfLeaving":"2021 (3 yrs)",
+        "Achievements":"Mining data from GitHub to analyze the participation of different social groups that exist in the communities formed on the platforms"
+      },
+      {
+        "CompanyName":"SKIO Music",
+        "specialization":"Full-Stack Software Developer (Co-op)",
+        "MonthOfLeaving":"May 2020 - December",
+        "YearOfLeaving":"2020 (7 mos)",
+        "Achievements":"Contributed to the product development of the team at SKIO Music, also analyzed user data to generate product performance reports"
+      },
+      {
+        "CompanyName":"University of Victoria",
+        "specialization":"Teaching Assistant",
+        "MonthOfLeaving":"Jan 2019 - April",
+        "YearOfLeaving":"2020 (1.5 yrs)",
+        "Achievements":"Taught: Software Evolution (SENG 371), Human Computer Interaction (SENG 310), Introduction to System Analysis (CSC 375), Software Development Methods (SENG 265)"
+      }
+    ],
+    "skillsDescription":" ",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Angular"
+      },
+      {
+        "skillname":"python"
+      }
+    ],
+    /*"portfolio":[
+      {
+        "name":"Loadstone",
+        "description":"A data visualization tool",
+        "imgurl":"images/portfolio/lodestone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],*/
+    "testimonials":[
+      {
+        "description":"As a software engineering co-op student, Trishala worked in our software development team at SKIO Music, initially focusing on front-end development and then expanding into the backend portion of our product as well. She contributed valuable work across the stack, from feature discussion and planning, to bug fixes and tweaks, as well as material new product features (which have shipped!) and enhancements to existing ones. We are still a startup, with rapid product development processes and many hats for each team member, but Trishala picked this up right away and jumped into our hectic workflow with meaningful contributions, while also learning and researching at the same time. She performed useful research and planning for a pathway to upgrade our AngularJS 1.x application to the more modern Angular 2+ framework, which we will be able to put into practice soon. She also contributed insights to product development and ideation, and performed great work with our product analytics. She helped build both the backend and frontend of new product features that are now providing new revenue streams for the company and value for our users. I would highly and happily recommend Trishala as an intelligent, driven, self-starting contributor in any team. She is a great, friendly team player, thinks things through and is always seeking to expand her knowledge and experience. It has been a distinct pleasure working with her, and I know she will be extremely effective as she continues in her career.",
+        "name":"Ben Cook, Technical Lead, SKIO Music Inc."
+      },
+      {
+        "description":"My association with Trishala dates back to June 2017 when she approached us for internship at our organization. During this three month internship she showed her absolute dedication to the assigned works and strong technical acumen. When I interviewed her I was impressed with her knowledge and confidence. So, I assigned her to work on an individual project that I usually give to a team. First, I considered that I should entrust her with the task of working on a project based on image processing as she is conversant with it. However, looking at her zeal to learn a new technology, I decided to assign her a project on Internet of Things. Besides gaining knowledge of product development the project helped her learn command parsing and machine to machine interaction. Hardworking and fervent, she accomplished the whole project singlehandedly and engaged herself in extensive research on techniques to make the conventional techniques that were used in the same domain better. Furthermore, she developed a project on GSM controlled Automated Shutter Control for ATM sites, where she coded on raspberry pi and simcom900 using python and simcom900 AT commands. The project has been implemented on various sites. Accountable and professional in her approach, Trishala completed all the tasks assigned to her on time. Reflecting deeply on issues, she leaves no stone unturned to find solutions to them. She is adept at applying her knowledge and skills in programming in real life situations. In a project on which she was working, there was a section which required at least 4 page code. She rigorously searched the web to find out portions where line of code could be curtailed. Commendably, she found out a library of python with which the task could be accomplished with only 4 lines. Even during breaks, she eagerly sought ways on the internet to improve output of her project. The final outcome of her unrelenting efforts and perseverance was a fully autonomous device which proved immensely helpful for us. During her tenure she showed remarkable improvement in the aspect of application of technologies. As I have closely assessed her work I can say that she is well prepared with competences that will allow her to serve the industry. She is bound to achieve heights of success in her chosen career. Her performance was at par with an experienced individual.  My peers were also impressed with the exemplary results that she delivered. Trishala has the required attributes and abilities that will help her excel in her future academic and professional undertakings.",
+        "name":"Azeet Kumar, Vice President IT, TSIPLC Ltd."
+      }
+    ]
+  }
+  
+  export default resumeData
