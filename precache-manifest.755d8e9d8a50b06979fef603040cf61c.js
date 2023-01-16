f2e10a2b223f7fd6eadaa9569e43bedb9698d53b
Trishala Bhasin <trishalabhasin@Trishalas-MacBook-Pro.local>
2023-01-15 18:13:59 -0800

    Updates

---
+++ precache-manifest.755d8e9d8a50b06979fef603040cf61c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8aab06bf79c7f253e043",
+    "url": "/my-portfolio-react/static/css/main.4b10daf7.chunk.css"
+  },
+  {
+    "revision": "8aab06bf79c7f253e043",
+    "url": "/my-portfolio-react/static/js/main.8aab06bf.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/my-portfolio-react/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "e79c74ac0783a1929ece",
+    "url": "/my-portfolio-react/static/js/runtime~main.e79c74ac.js"
+  },
+  {
+    "revision": "2cbdb8f75eea0530ae1040dd559fe3bc",
+    "url": "/my-portfolio-react/index.html"
+  }
+];
